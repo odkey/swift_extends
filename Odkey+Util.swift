cec0ca45467f212a7ab238b14ff11f6b80f17bdc
odkey <amglowez@gmail.com>
2017-10-02 19:20:07 +0900

    Added: Numeric helper functions

---
+++ Odkey+Util.swift
@@ -0,0 +1,36 @@
+//
+//  Odkey+Util.swift
+//
+//  Created by Yota Odaka on 2017/10/02.
+//  Copyright © 2017 Yota Odaka. All rights reserved.
+//
+
+import Foundation
+
+class Odkey {
+
+  // MARK: - Numeric
+  class Numeric {
+    static let pi = Double.pi
+
+    static func randomNumber(_ min: Float, _ max: Float) -> Float {
+      return Float(arc4random_uniform(UINT32_MAX)) / Float(UINT32_MAX) *
+               (max - min) + min
+    }
+    
+    static func random0To1F() -> Float {
+      return randomNumber(0.0, 1.0)
+    }
+    
+    static func randomInt(_ min: Float, _ max: Float) -> Int {
+      return Int(randomNumber(min, max))
+    }
+    
+    static func mapBetween0To1(_ value: Float,
+                               min: Float, max: Float) -> Float {
+      return value / (max - min)
+    }
+    
+  } // class Numeric
+
+} // class Odkey
